056bd6c9798a46eec0a3336d2d63bcd0e8e07acc
web surf <>
2020-02-23 22:25:42 -0600

    Face lift and 'match word' checkbox

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,114 @@
 <script>
-	let searchTerm
+	import Loader from './Components/Loader.svelte'
+	import { fly } from 'svelte/transition'
+
+	let searchTerm = ''
 	let jokes = []
+	let loading = false
+	let matchWholeWord = false
+	const jokeError = [{joke: "No matching jokes found.", error: true}]
 
-	const searchJokes = async (term) => {
-		const res = await fetch(`/.netlify/functions/search-jokes?term=${term}`)
-		const resJSON = await res.json()
-		jokes = [...resJSON.data.results]
+	$: invalidSearch = searchTerm.length < 2
+
+	const searchJokes = (term) => {
+		loading = true
+		jokes = []
+		fetch(`/.netlify/functions/search-jokes?term=${term}`)
+			.then(res => res.json())
+			.then(json => {
+				if (json.data.results.length) {
+					jokes = [...json.data.results]
+					if (matchWholeWord) {
+						const matchesWholeWord = new RegExp(` ${term} `)
+						jokes = [...jokes.filter(joke => matchesWholeWord.test(joke.joke))]
+						if (!jokes.length) jokes = jokeError
+					}
+				} else {
+					jokes = jokeError
+				}
+				loading = false
+		})
 	}
 
-	const randomJoke = async () => {
-		const res = await fetch('/.netlify/functions/random-joke')
-		const resJSON = await res.json();
-		jokes = [resJSON.data]
-	}
+	const randomJoke = () => {
+		loading = true
+		jokes = []
+		const res = fetch('/.netlify/functions/random-joke')
+			.then(res => res.json())
+			.then(json => {
+				if(json.data) {
+					jokes = [json.data]
+				} else {
+					jokes = jokeError
+				}
+				loading = false
+			})
+		}
 </script>
 
 <main>
-	<h1>Svelte Node App</h1>
-	<form on:submit|preventDefault={searchJokes(searchTerm)}>
-		<label for="search-term"></label>
-		<input bind:value={searchTerm} type="text" id="search-term">
-		<button>Search Jokes</button>
-		<button type="button" on:click|preventDefault={randomJoke}>Random Joke</button>
-	</form>
+	<div class="container">
+		<h1>Search Dad Jokes:</h1>
+		<form on:submit|preventDefault={searchJokes(searchTerm)}>
+			<input bind:value={searchTerm} type="text" id="search-term">
+			<button disabled={invalidSearch}>Search Jokes</button>
 
-	{#each jokes as joke}
-		<p>{joke.joke}</p>
-	{/each}
+			<div>
+				<input type="checkbox" id="whole-word" bind:checked={matchWholeWord}>
+				<label for="whole-word">Match whole word</label>
+			</div>
+
+		</form>
+
+		<p class="or">or</p>
+
+		<button type="button" on:click|preventDefault={randomJoke}>Get a random joke</button>
+
+		{#if loading}
+			<Loader />
+		{/if}
+
+		<ul>
+			{#each jokes as joke, index}
+				<li in:fly="{{ y: 20, duration: 400, delay: index * 120}}" class:error={joke.error}>
+					{joke.joke}
+				</li>
+			{/each}
+		</ul>
+	</div>
 </main>
 
 <style>
 	main {
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
 		margin: 0 auto;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.or {
+		font-size: 1.5rem;
+		font-weight: bold;
+	}
+
+	.error {
+		color: red;
+		font-weight: bold;
+	}
+
+	.or:before, .or:after {
+		content: '—';
+		margin: 0 .25em;
+	}
+
+	ul {
+		text-align: left;
+		list-style-type: none;
+		margin: 3rem auto;
+	}
+
+	li {
+		margin-bottom: 1em;
+		padding-bottom: 1em;
+		border-bottom: 1px solid #ccc;
 	}
 </style>
---
+++ src/Components/Loader.svelte
@@ -0,0 +1,44 @@
+<div id="loader">
+	<div class="dot"></div>
+	<div class="dot"></div>
+	<div class="dot"></div>
+</div>
+
+
+<style>
+#loader {
+	display: flex;
+	justify-content: center;
+	text-align: center;
+	margin: 1em 0;
+}
+
+.dot {
+	width: 1em;
+	height: 1em;
+	border-radius: 1em;
+	margin: .1em;
+	background: #69e;
+	animation: pulse .9s infinite ease;
+}
+
+.dot:nth-of-type(2) {
+	animation-delay: .15s;
+}
+
+.dot:nth-of-type(3) {
+	animation-delay: .3s;
+}
+
+@keyframes pulse {
+	0% {
+		transform: scale(.2)
+	}
+	50% {
+		transform: scale(1)
+	}
+	100% {
+		transform: scale(.2)
+	}
+}
+</style>
